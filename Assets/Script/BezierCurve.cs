46825f98b12cbfbdfd6ef40312a0f2e2f0969834
SeyedMortezaKamali <smkplus3d@gmail.com>
2019-04-30 19:08:13 +0430

    Initial commit

---
+++ Assets/Script/BezierCurve.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+public class BezierCurve : MonoBehaviour {
+
+	public Vector3[] points;
+
+	public Transform StartPoint,Target;
+	
+	private void Start() {
+		transform.position = StartPoint.position;
+		points[3] = Target.position - transform.position;
+		var direction = Target.position - transform.position;
+		//if(direction.x < 0){
+			points[1].x *= Random.Range(0,2)*2-1;//Random Between -1 and 1
+			points[2].x *= Random.Range(0,2)*2-1;//Random Between -1 and 1
+		//}
+	}
+	public Vector3 GetPoint (float t) {
+		return transform.TransformPoint(Bezier.GetPoint(points[0], points[1], points[2], points[3], t));
+	}
+	
+	public Vector3 GetVelocity (float t) {
+		return transform.TransformPoint(Bezier.GetFirstDerivative(points[0], points[1], points[2], points[3], t)) - transform.position;
+	}
+	
+	public Vector3 GetDirection (float t) {
+		return GetVelocity(t).normalized;
+	}
+	
+	public void Reset () {
+		points = new Vector3[] {
+			new Vector3(1f, 0f, 0f),
+			new Vector3(2f, 0f, 0f),
+			new Vector3(3f, 0f, 0f),
+			new Vector3(4f, 0f, 0f)
+		};
+	}
+}
---
+++ Assets/Script/Coin.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coin : MonoBehaviour {
+	public BezierCurve curve;
+	float _timer;
+	float _randomSpeed;
+
+	void Start () {
+		_randomSpeed = Random.Range(0.7f,1);
+	}
+	
+	void Update () {
+		_timer += Time.deltaTime*_randomSpeed;
+		transform.position = curve.GetPoint(_timer);
+	}
+}
